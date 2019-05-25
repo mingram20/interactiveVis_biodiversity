7c14cbc2e6b7ff4f5374748b8e09c3c4a7ec8ef1
mingram20 <michael.ingram87@gmail.com>
2019-05-25 14:38:43 -0400

    modified procfile

--- Belly_Button_Biodiversity/app.py
+++ Belly_Button_Biodiversity/app.py
@@ -13,8 +13,6 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-
 #################################################
 # Database Setup
 #################################################
